4151ef44bee424d37f792f6cd993d0c54eaa6887
djBrandy <dandobrandon0@gmail.com>
2024-03-22 09:17:25 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264; // 1 block is equivalent to 264 feet
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+    
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
